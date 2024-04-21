743bd0eaeb6b44c9b37b51a094441bfcd8a981b9
QH615NR <QH615NR@IN2410268W1.ey.net>
2024-04-21 14:46:36 +0530

    purchase order commit

---
+++ Test Suites/Swag_lab/Purchase_product.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Purchase_product</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6bce1b4e-b12b-4c8a-859c-32ad1603178d</testSuiteGuid>
+   <testCaseLink>
+      <guid>eecaef01-63e4-4779-ac45-0a4ef58801e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag_Lab/Purchase_product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
