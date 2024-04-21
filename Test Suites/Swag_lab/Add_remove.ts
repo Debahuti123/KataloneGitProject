50724969385980b9e5af0e4364f30a891de0dce2
QH615NR <QH615NR@IN2410268W1.ey.net>
2024-04-21 15:28:13 +0530

    add and remove testcase has been commited

---
+++ Test Suites/Swag_lab/Add_remove.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Add_remove</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3cdc5868-b902-4bb7-b784-b60c4f062776</testSuiteGuid>
+   <testCaseLink>
+      <guid>9f94504b-50b6-454d-9732-4ff51c1a724d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag_Lab/Add_remove</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
