038b014fada0b5130ef15caa03b1215ae47859e5
QH615NR <QH615NR@IN2410268W1.ey.net>
2024-04-24 18:46:01 +0530

    Remove_item commited

---
+++ Test Suites/Swag_lab/Remove_item.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Remove_item</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3f4542dc-185a-4087-9c38-d84697172051</testSuiteGuid>
+   <testCaseLink>
+      <guid>34f241ad-4f84-4506-ab65-aeccb3b2c116</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag_Lab/Remove_item</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
