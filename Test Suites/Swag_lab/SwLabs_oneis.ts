273618d2a3ba7982041021a44eb6e17548e67259
QH615NR <QH615NR@host.docker.internal>
2024-04-28 15:00:15 +0530

    commited oneis

---
+++ Test Suites/Swag_lab/SwLabs_oneis.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SwLabs_oneis</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>af5203c3-9e87-46e8-8840-6a6548a241fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>32af7647-2597-422b-af79-f8d89cbde30c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag_Lab/SwLabs_Onesie</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
